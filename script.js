8b5c70795cdc6fcc78d6385e6d7bd40e5af9fbbe
Juan-Ceballos <juanceballos@pursuit.org>
2020-07-09 20:54:43 -0400

    first js project Hello World

---
+++ script.js
@@ -0,0 +1,7 @@
+let element = document.createElement("h1");
+let span = document.createElement("span");
+
+span.innerText = "Hello World";
+element.appendChild(span);
+document.body.appendChild(element);
+// fetch("http://localhost:3000");
